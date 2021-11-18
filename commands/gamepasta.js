fac247d0820f35e5b561639f36b66a064198c7dd
sadnixon <askspencerhill@berkeley.edu>
2021-11-18 12:49:19 -0800

    changing back to old pasta

--- commands/gamepasta.js
+++ commands/gamepasta.js
@@ -5,6 +5,12 @@ const { errorMessage, rank } = require("../message-helpers");
 
 async function execute(message, args, user) {
   if (user.isAuthorized) {
+    
+    if (args[0] == null) {
+      return message.channel.send(errorMessage("No link given."));
+    }
+    const link = args[0];
+    
     var channels = await team_roles_channels.get("teams");
     channels = channels.map((team) => team[1]);
     channels = channels.concat(["599756425241296897", "697225108376387724"]); //channel ids for event-general-chat and tournament-vc-text
@@ -22,13 +28,13 @@ async function execute(message, args, user) {
         message.guild.channels.cache
           .get(channel)
           .send(
-            `The stream with the game is about to start. :warning: :no_entry: Remember to be respectful to all players and not to make comments about the game here or where players can see it, thanks! :warning: :no_entry: If you want to spectate and listen in on the game join the VC GAMES voice channel. Note, that you will be muted in that VC but you will be able to chat in #tournament-vc-text per usual. Enjoy!`
+            `You're about to receive the link to the game. :warning: :no_entry: Remember to be respectful to all players and not to make comments about the game here or where players can see it, thanks! :warning: :no_entry: If you want to spectate and listen in on the game join the VC GAMES voice channel. Note, that you will be muted in that VC but you will be able to chat in #tournament-vc-text per usual. Enjoy! ${link}`
           );
       } else {
         message.guild.channels.cache
           .get(channel)
           .send(
-            `The stream with the game is about to start. :warning: :no_entry: Remember not to make any comment about the game here or where players can see it, thanks! :warning: :no_entry: If you want to spectate and chat please join the Tournament VC and limit all chat to #tournament-vc-text. After the conclusion of the game, please be respectful to all of the players. Enjoy!`
+            `You're about to receive the link to the game. :warning: :no_entry: Remember not to make any comment about the game here or where players can see it, thanks! :warning: :no_entry: If you want to spectate and chat please join the Tournament VC and limit all chat to #tournament-vc-text. After the conclusion of the game, please be respectful to all of the players. Enjoy! ${link}`
           );
       }
     }
