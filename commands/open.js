5d12cc0aaca2cee681dca23b34a4926b7b697f22
sadnixon <askspencerhill@berkeley.edu>
2023-07-14 16:02:00 -0700

    fixing hack fix

--- commands/open.js
+++ commands/open.js
@@ -6,7 +6,7 @@ async function execute(message, args, user) {
   const gameNumber = await getGameNumber();
   const games2 = await sheet.getGames();
   const currentGame = games2.find((g) => !g.played);
-  const currentMode = 'AvalonSH';
+  //const currentMode = 'AvalonSH';
   console.log(currentGame);
   if (!(await guess_information.get("open")) && user.isAuthorized) {
     message.channel.send("Guessing Opened!");
@@ -17,10 +17,10 @@ async function execute(message, args, user) {
     await guess_information.set("specialGuessIDs", []);
     //guessDict = {};
     if (
-      ["Anonspecial", "AvalonSH", "AvalonSH+"].includes(currentMode)
+      ["Anonspecial", "AvalonSH", "AvalonSH+"].includes(currentGame.mode)
     ) {
       await guess_information.set("openSpecial", true);
-      await guess_information.set("specialMode", currentMode);
+      await guess_information.set("specialMode", currentGame.mode);
     } else {
       await guess_information.set("openSpecial", false);
       await guess_information.set("specialMode", false);
